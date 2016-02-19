71c4956dd604ac3e0014bf50baf0c20bd1f15da1
Erik Jung <erik@cloudfour.com>
2016-02-18 16:18:51 -0800

    Add more caching related functions

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,40 +3,45 @@
 importScripts('SMCacheUtils.js');
 
 const VERSION = '0.0.1';
+const TYPES = ['image', 'content'];
+const TYPES_DEFAULT = 'static';
 const cachePaths = [
-  '/example.css'
+  '/example.css',
+  '/jason.png'
 ];
 
 const cacheName = key => `${VERSION}-${key}`;
+const isRequest = obj => obj instanceof Request;
+const isResponse = obj => obj instanceof Response;
 const isCacheUrl = url => cachePaths.includes(url.pathname);
-const isGetRequest = request => request.method === 'GET';
 const isLocalUrl = url => url.origin === location.origin;
+const isGetRequest = request => request.method === 'GET';
+const requestType = request => request.headers.get('Accept');
+const responseType = response => response.headers.get('Content-Type');
 
-function addToCache (cacheKey, request, response) {
-  if (response.ok) {
-    var copy = response.clone();
-    caches.open(cacheKey).then(cache => {
-      cache.put(request, copy);
-    });
-  }
+const resourceType = function resourceType (obj) {
+  const mime = (isRequest(obj) ? requestType : responseType)(obj);
+  return TYPES.find(type => mime.includes(type)) || TYPES_DEFAULT;
+};
+
+const addToCache = function addToCache (request, response) {
+  const cacheKey = cacheName(resourceType(response));
+  caches.open(cacheKey).then(
+    cache => cache.put(request, response.clone())
+  );
   return response;
-}
+};
 
-function fetchFromCache (request) {
-  return caches.match(request).then(response => {
-    if (!response) {
-      throw Error(`${request.url} not found in cache`);
-    }
-    return response;
-  });
-}
+const getFromCache = function getFromCache (request) {
+  return caches.match(request);
+};
 
 addEventListener('install', event => {
-
+  // precache stuff
 });
 
 addEventListener('activate', event => {
-
+  // invalidate old caches
 });
 
 addEventListener('fetch', event => {
@@ -44,6 +49,6 @@ addEventListener('fetch', event => {
   const url = new URL(request.url);
 
   if (isCacheUrl(url)) {
-
+    // respond from cache, adding to it first if needed
   }
 });
