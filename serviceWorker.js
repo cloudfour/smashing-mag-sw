71f21d339450f5622800dc6992b51e4de7b40f29
Erik Jung <erik@cloudfour.com>
2016-02-26 14:43:58 -0800

    Improve comments for doxx usage

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,6 @@
-/* global self, caches, URL, Request, Response */
-/* eslint indent: [2, 2] */
-/* eslint-env es6 */
-
+/**
+ * @ignore
+ */
 'use strict';
 
 const VERSION = '0.0.1';
@@ -26,14 +25,12 @@ const getRequestTypeHeader = curry(getHeader, 'Accept');
 const getResponseTypeHeader = curry(getHeader, 'Content-Type');
 
 /**
- * getResourceTypeHeader receives a Request or Response instance, and it returns
- * a header value indicating the MIME-type of that object.
+ * `getResourceTypeHeader()` receives a `Request` or `Response` instance, and it
+ * returns a header value indicating the MIME-type of that object.
  *
  * @param {Request|Response} obj
  * @return {String}
- * @example
- *
- *    getResourceTypeHeader(cssRequest); // => 'text/css'
+ * @example getResourceTypeHeader(cssRequest); // => 'text/css'
  */
 const getResourceTypeHeader = obj => {
   if (isRequest(obj)) return getRequestTypeHeader(obj);
@@ -41,14 +38,12 @@ const getResourceTypeHeader = obj => {
 };
 
 /**
- * contentType receives a Request or Response instance, and it returns a generic
- * string alias for the MIME-type of that object.
+ * `contentType()` receives a `Request` or `Response` instance, and it returns a
+ * generic string alias for the MIME-type of that object.
  *
  * @param {Request|Response} obj
  * @return {String}
- * @example
- *
- *    contentType(new Request('foo.html')); // => 'content'
+ * @example contentType(new Request('foo.html')); // => 'content'
  */
 const contentType = obj => {
   const typeHeader = getResourceTypeHeader(obj);
@@ -63,19 +58,17 @@ const contentType = obj => {
 };
 
 /**
- * isCacheableURL receives a URL instance and returns true or false depending on
- * whether or not:
+ * `isCacheableURL()` receives a `URL` instance and returns `true` or `false`
+ * depending on whether or not:
  *
- * - its pathname exists within the `cacheablePaths` array
- * - its value matches the `cacheablePattern` regex
+ * - its `pathname` exists within the `cacheablePaths` array
+ * - its value matches the `cacheablePattern` pattern
  *
  * TODO: Clean up that nasty replacement regex (for GH Pages)
  *
- * @param {URL}
+ * @param {URL} url
  * @return {Boolean}
- * @example
- *
- *    isCacheableURL(new URL('example.com/nope')); // => false
+ * @example isCacheableURL(new URL('example.com/nope')); // => false
  */
 const isCacheableURL = url => {
   const path = url.pathname.replace(/(\/)(smashing-mag-sw\/)?/, '');
@@ -85,15 +78,12 @@ const isCacheableURL = url => {
 };
 
 /**
- * isCacheableRequest receives a Request instance and returns true or false
- * depending on the properties of its URL and header values.
+ * `isCacheableRequest()` receives a `Request` instance and returns `true` or
+ * `false` depending on the properties of its URL and header values.
  *
  * @param {Request} request
  * @return {Boolean}
- * @example
- *
- *    isCacheableRequest(siteLogoRequest); // => true
- *    isCacheableRequest(thirdPartyScriptRequest); // => false
+ * @example isCacheableRequest(new Request('logo.svg')); // => true
  */
 const isCacheableRequest = request => {
   const url = new URL(request.url);
@@ -106,15 +96,13 @@ const isCacheableRequest = request => {
 };
 
 /**
- * openCache optionally receives one or more string arguments used to construct
- * a key for caches.open(). If no arguments are supplied, the `VERSION` constant
- * alone will be used as the cache key.
+ * `openCache()` optionally receives one or more string arguments used to
+ * construct a key for `caches.open()`. If no arguments are supplied, the
+ * `VERSION` constant alone will be used as the cache key.
  *
- * @param {...String}
+ * @param {...String} args
  * @return {Promise}
- * @example
- *
- *    openCache('images').then(cache => ...); // key is "0.0.0-images"
+ * @example openCache('images').then(cache => ...); // key is "0.0.0-images"
  */
 const openCache = (...args) => {
   const key = [VERSION].concat(args).join('-');
@@ -122,6 +110,7 @@ const openCache = (...args) => {
 };
 
 /**
+ * @ignore
  * This is the installation handler. It runs when the worker is first installed.
  * It precaches the asset paths in the `cacheablePaths` array.
  */
@@ -134,6 +123,7 @@ self.addEventListener('install', event => {
 });
 
 /**
+ * @ignore
  * This is the activation handler. It runs after the worker is installed. It
  * handles the deletion of stale cache responses.
  */
@@ -150,20 +140,22 @@ self.addEventListener('activate', event => {
 });
 
 /**
+ * @ignore
  * This is the fetch handler. It runs upon every request, but it only acts upon
  * requests that return true when passed to `isCacheableRequest`. It both
  * serves requests from the cache and adds requests to the cache.
+ *
+ * This is tricky:
+ * https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/match
+ * `.match()` does not seem to throw anything when no matching items are found.
+ * So instead of using `.catch()` here, we use `.then()` and check the value of
+ * response (which could be undefined).
  */
 self.addEventListener('fetch', event => {
   const request = event.request;
   if (isCacheableRequest(request)) {
     event.respondWith(
-      /**
-       * This is tricky:
-       * https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/match
-       * Opposed to these docs, .match() does not seem to throw anything when no
-       * matching items are found. So instead of using .catch() here, we use
-       * .then() and check the value of response (which could be undefined).
+
        */
       caches.match(request).then(response => {
         // The request was found in the cache; return it.
